a524059df64e98159086bf80fea8098c98c2daf6
kpfarn <kevin.farnsworth.23@cnu.edu>
2026-09-23 16:58:23 -0400

    started left panel

--- layout.py
+++ layout.py
@@ -1,13 +1,22 @@
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("Dark")
+ctk.set_appearance_mode("Dark")
 
 #GLOBAL DEFAULTS
 font = ("arial", 20)
 color1 = "RED"
 color2 = "ORANGE"
 
-app = customtkinter.CTk() 
+
+#LEFT STOCK PANEL
+class MyFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.label = ctk.CTkLabel(self, text="STOCKS", text_color="white")
+        self.label.grid(row=0, column=0, padx=20)
+
+app = ctk.CTk() 
 
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
@@ -18,26 +27,27 @@ def button_function():
     print("button pressed")
 
 #LABEL TO INDICATE ACTION
-label = customtkinter.CTkLabel(app, text="What can I help you with?", fg_color="transparent", text_color="white", font=font)
+label = ctk.CTkLabel(app, text="What can I help you with?", fg_color="transparent", text_color="white", font=font)
 label.place(relx=0.55, rely=0.7)
 
 
 #CONVERSATION OPTION BUTTONS
-button = customtkinter.CTkButton(master=app, text="OPTION 1", font=font, 
-                                 fg_color=color1, command=button_function, height=100, width=400)
+button = ctk.CTkButton(master=app, text="OPTION 1", font=font, 
+                                 fg_color=color1, height=100, width=400)
 button.place(relx=0.25, rely=0.8)
 
-button = customtkinter.CTkButton(master=app, text="OPTION 2", font=font, 
-                                 fg_color=color1, command=button_function, height=100, width=400)
+button = ctk.CTkButton(master=app, text="OPTION 2", font=font, 
+                                 fg_color=color1, height=100, width=400)
 button.place(relx=0.5, rely=0.8)
 
-button = customtkinter.CTkButton(master=app, text="OPTION 3", font=font, 
-                                 fg_color=color1, command=button_function, height=100, width=400)
+button = ctk.CTkButton(master=app, text="OPTION 3", font=font, 
+                                 fg_color=color1, height=100, width=400)
 button.place(relx=0.75, rely=0.8)
 
-#LEFT STOCK PANEL
-ctk_textbox_scrollbar = customtkinter.CTkScrollbar(app)
-ctk_textbox_scrollbar.grid(row=0, column=700, sticky="ns")
+#INSTANTIATE THE LEFT SCROLLER
+frame = MyFrame(app, width=250, fg_color=color2)
+frame.pack(side="left", fill="y", padx=10, pady=10)
+frame.label.configure(text="PORTFOLIO")
 
 
 app.mainloop()
